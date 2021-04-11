d26dc4b74eb159493bc007da9612df344cf41b68
Dario Mambro <unevens@users.noreply.github.com>
2021-04-11 07:44:55 +0200

    handling failure at loading view data

--- unplug/include/unplug/ViewPersistentData.hpp
+++ unplug/include/unplug/ViewPersistentData.hpp
@@ -25,11 +25,69 @@ struct ViewPersistentData
   std::vector<std::string> strings;
   std::vector<char> bytes;
 
+  template<class SaveInteger, class SaveIntegerArray, class SaveDoubleArray, class SaveBytes>
+  bool save(SaveInteger saveInteger,
+            SaveIntegerArray saveIntegerArray,
+            SaveDoubleArray saveDoubleArray,
+            SaveBytes saveBytes) const
+  {
+    {
+      auto const numIntegersToSave = static_cast<int64_t>(integers.size());
+      if (!saveInteger(numIntegersToSave))
+        return false;
+      if (!saveIntegerArray(integers.data(), numIntegersToSave))
+        return false;
+    }
+    {
+      auto const numNumbersToSave = static_cast<int64_t>(numbers.size());
+      if (!saveInteger(numNumbersToSave))
+        return false;
+      if (!saveDoubleArray(numbers.data(), numNumbersToSave))
+        return false;
+    }
+    {
+      auto const numStringsToSave = static_cast<int64_t>(strings.size());
+      if (!saveInteger(numStringsToSave))
+        return false;
+      for (int i = 0; i < numStringsToSave; ++i) {
+        auto const cString = strings[i].c_str();
+        auto const stringLength = static_cast<int64_t>(strlen(cString));
+        if (!saveInteger(stringLength))
+          return false;
+        if (!saveBytes(cString, stringLength))
+          return false;
+      }
+    }
+    {
+      auto const numBytesToSave = static_cast<int64_t>(bytes.size());
+      if (!saveInteger(numBytesToSave))
+        return false;
+      if (!saveBytes(bytes.data(), numBytesToSave))
+        return false;
+    }
+    return true;
+  }
+
   template<class LoadInteger, class LoadIntegerArray, class LoadDoubleArray, class LoadBytes>
   bool load(LoadInteger loadInteger,
             LoadIntegerArray loadIntegerArray,
             LoadDoubleArray loadDoubleArray,
             LoadBytes loadBytes)
+  {
+    auto copy = ViewPersistentData{};
+    bool const loaded = copy.loadInternal(loadInteger, loadIntegerArray, loadDoubleArray, loadBytes);
+    if (loaded) {
+      *this = copy;
+    }
+    return loaded;
+  }
+
+private:
+  template<class LoadInteger, class LoadIntegerArray, class LoadDoubleArray, class LoadBytes>
+  bool loadInternal(LoadInteger loadInteger,
+                    LoadIntegerArray loadIntegerArray,
+                    LoadDoubleArray loadDoubleArray,
+                    LoadBytes loadBytes)
   {
     {
       int64_t numIntegersToLoad = 0;
@@ -70,49 +128,6 @@ struct ViewPersistentData
         return false;
       bytes.resize(numBytesToLoad);
       if (!loadBytes(bytes.data(), numBytesToLoad))
-        return false;
-    }
-    return true;
-  }
-
-  template<class SaveInteger, class SaveIntegerArray, class SaveDoubleArray, class SaveBytes>
-  bool save(SaveInteger saveInteger,
-            SaveIntegerArray saveIntegerArray,
-            SaveDoubleArray saveDoubleArray,
-            SaveBytes saveBytes) const
-  {
-    {
-      auto const numIntegersToSave = static_cast<int64_t>(integers.size());
-      if (!saveInteger(numIntegersToSave))
-        return false;
-      if (!saveIntegerArray(integers.data(), numIntegersToSave))
-        return false;
-    }
-    {
-      auto const numNumbersToSave = static_cast<int64_t>(numbers.size());
-      if (!saveInteger(numNumbersToSave))
-        return false;
-      if (!saveDoubleArray(numbers.data(), numNumbersToSave))
-        return false;
-    }
-    {
-      auto const numStringsToSave = static_cast<int64_t>(strings.size());
-      if (!saveInteger(numStringsToSave))
-        return false;
-      for (int i = 0; i < numStringsToSave; ++i) {
-        auto const cString = strings[i].c_str();
-        auto const stringLength = static_cast<int64_t>(strlen(cString));
-        if (!saveInteger(stringLength))
-          return false;
-        if (!saveBytes(cString, stringLength))
-          return false;
-      }
-    }
-    {
-      auto const numBytesToSave = static_cast<int64_t>(bytes.size());
-      if (!saveInteger(numBytesToSave))
-        return false;
-      if (!saveBytes(bytes.data(), numBytesToSave))
         return false;
     }
     return true;

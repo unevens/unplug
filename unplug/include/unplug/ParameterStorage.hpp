f5809d4f9031aaaf40cc4cc5959dbfcf7b3a78a8
Dario Mambro <unevens@users.noreply.github.com>
2021-09-05 15:45:48 +0200

    memory barriers

--- unplug/include/unplug/ParameterStorage.hpp
+++ unplug/include/unplug/ParameterStorage.hpp
@@ -24,18 +24,9 @@ class ParameterStorage final
   class ParameterNormalization final
   {
   public:
-    explicit ParameterNormalization(ParameterValueType min = 0, ParameterValueType max = 1)
-      : range(max - min)
-      , offset(min)
-    {}
-    ParameterValueType toNormalized(ParameterValueType x) const
-    {
-      return (x - offset) * range;
-    }
-    ParameterValueType fromNormalized(ParameterValueType x) const
-    {
-      return x * range + offset;
-    }
+    explicit ParameterNormalization(ParameterValueType min = 0, ParameterValueType max = 1);
+    ParameterValueType toNormalized(ParameterValueType x) const;
+    ParameterValueType fromNormalized(ParameterValueType x) const;
 
   private:
     ParameterValueType offset;
@@ -49,52 +40,96 @@ class ParameterStorage final
   };
 
 public:
-  void set(int index, ParameterValueType value)
-  {
-    parameters[index].value.store(value);
-  }
+  void set(int index, ParameterValueType value);
 
-  ParameterValueType get(int index) const
-  {
-    return parameters[index].value.load();
-  }
+  ParameterValueType get(int index) const;
 
-  void setNormalized(int index, ParameterValueType value)
-  {
-    parameters[index].value.store(parameters[index].convert.fromNormalized(value));
-  }
+  void setNormalized(int index, ParameterValueType value);
 
-  ParameterValueType getNormalized(int index) const
-  {
-    return parameters[index].convert.toNormalized(parameters[index].value.load());
-  }
+  ParameterValueType getNormalized(int index) const;
 
-  void initialize(std::vector<ParameterDescription> const& parameterDescriptions)
-  {
-    initializeConversions(parameterDescriptions);
-    initializeDefaultValues(parameterDescriptions);
-  }
+  void initialize(std::vector<ParameterDescription> const& parameterDescriptions);
 
 private:
-  void initializeConversions(std::vector<ParameterDescription> const& parameterDescriptions)
-  {
-    int i = 0;
-    for (auto& parameter : parameterDescriptions) {
-      auto const min = parameter.isNonlinear() ? parameter.nonlinearToLinear(parameter.min) : parameter.min;
-      auto const max = parameter.isNonlinear() ? parameter.nonlinearToLinear(parameter.max) : parameter.max;
-      parameters[i].convert = ParameterNormalization{ min, max };
-      ++i;
-    }
-  }
+  void initializeConversions(std::vector<ParameterDescription> const& parameterDescriptions);
 
-  void initializeDefaultValues(std::vector<ParameterDescription> const& parameterDescriptions)
-  {
-    for (int i = 0; i < parameterDescriptions.size(); ++i) {
-      parameters[i].value.store(parameterDescriptions[i].defaultValue);
-    }
-  }
+  void initializeDefaultValues(std::vector<ParameterDescription> const& parameterDescriptions);
 
   std::array<StoredParameter, numParameters> parameters;
 };
+
+// implementation
+
+template<int numParameters>
+void ParameterStorage<numParameters>::set(int index, ParameterValueType value)
+{
+  parameters[index].value.store(value, std::memory_order_relaxed);
+}
+
+template<int numParameters>
+ParameterValueType ParameterStorage<numParameters>::get(int index) const
+{
+  return parameters[index].value.load(std::memory_order_relaxed);
+}
+
+template<int numParameters>
+void ParameterStorage<numParameters>::setNormalized(int index, ParameterValueType value)
+{
+  set(index, parameters[index].convert.fromNormalized(value));
+}
+
+template<int numParameters>
+ParameterValueType ParameterStorage<numParameters>::getNormalized(int index) const
+{
+  return parameters[index].convert.toNormalized(get(index));
+}
+
+template<int numParameters>
+void ParameterStorage<numParameters>::initialize(const std::vector<ParameterDescription>& parameterDescriptions)
+{
+  initializeConversions(parameterDescriptions);
+  initializeDefaultValues(parameterDescriptions);
+}
+
+template<int numParameters>
+void ParameterStorage<numParameters>::initializeConversions(
+  const std::vector<ParameterDescription>& parameterDescriptions)
+{
+  int i = 0;
+  for (auto& parameter : parameterDescriptions) {
+    auto const min = parameter.isNonlinear() ? parameter.nonlinearToLinear(parameter.min) : parameter.min;
+    auto const max = parameter.isNonlinear() ? parameter.nonlinearToLinear(parameter.max) : parameter.max;
+    parameters[i].convert = ParameterNormalization{ min, max };
+    ++i;
+  }
+}
+
+template<int numParameters>
+void ParameterStorage<numParameters>::initializeDefaultValues(
+  const std::vector<ParameterDescription>& parameterDescriptions)
+{
+  for (int i = 0; i < parameterDescriptions.size(); ++i) {
+    parameters[i].value.store(parameterDescriptions[i].defaultValue);
+  }
+}
+
+template<int numParameters>
+ParameterStorage<numParameters>::ParameterNormalization::ParameterNormalization(ParameterValueType min,
+                                                                                ParameterValueType max)
+  : range(max - min)
+  , offset(min)
+{}
+
+template<int numParameters>
+ParameterValueType ParameterStorage<numParameters>::ParameterNormalization::toNormalized(ParameterValueType x) const
+{
+  return (x - offset) * range;
+}
+
+template<int numParameters>
+ParameterValueType ParameterStorage<numParameters>::ParameterNormalization::fromNormalized(ParameterValueType x) const
+{
+  return x * range + offset;
+}
 
 } // namespace unplug

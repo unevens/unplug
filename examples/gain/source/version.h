1b5cffbf5daef1e2126d7452c4b1a00042287724
Dario Mambro <unevens@users.noreply.github.com>
2021-10-15 05:15:55 +0200

    fixes for windows

--- examples/gain/source/version.h
+++ examples/gain/source/version.h
@@ -8,6 +8,12 @@
 #define UNPLUG_PLUGIN_VENDOR_URL "https://unevens.net"
 #define UNPLUG_PLUGIN_VENDOR_MAIL "mailto:hi@unevens.net"
 #define stringLegalCopyright "Copyright(c) 2021 Dario Mambro."
+#define stringOriginalFilename "UnplugGainExample" ".vst3"
+#if SMTG_PLATFORM_64
+#define stringFileDescription "UnplugGainExample" " VST3 (64Bit)"
+#else
+#define stringFileDescription "UnplugGainExample" " VST3"
+#endif
 
 // Here you can define the version of your plug-in: "Major.Sub.Release.Build""
 #define MAJOR_VERSION_STR "1"
@@ -22,19 +28,6 @@
 #define BUILD_NUMBER_STR "1" // Build number to be sure that each result could be identified.
 #define BUILD_NUMBER_INT 1
 
-// you don't need to change the next macros
-
-#define stringOriginalFilename (stringPluginName ".vst3")
-#if SMTG_PLATFORM_64
-#define stringFileDescription (stringPluginName " VST3 (64Bit)")
-#else
-#define stringFileDescription (stringPluginName " VST3")
-#endif
-
-// Version with build number (example "1.0.3.342")
 #define FULL_VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
-
-// Version without build number (example "1.0.3")
 #define VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR
-
 #define stringLegalTrademarks "VST is a trademark of Steinberg Media Technologies GmbH"
